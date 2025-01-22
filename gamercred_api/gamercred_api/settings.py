c20c7fa856009dce385147abd2fb1088f8400102
C0R4L0V3 <coraline.e.love@gmail.com>
2025-01-22 17:50:59 -0600

    fixed typo error in users login view

--- gamercred_api/gamercred_api/settings.py
+++ gamercred_api/gamercred_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s&!rz9u+oben6+tr!3@$j$u5puyvtzsycb=0byf87#&e0bn43x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
