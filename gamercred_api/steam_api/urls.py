3dd560030e5d3aabac4147df59e19d4325b89b3a
C0R4L0V3 <coraline.e.love@gmail.com>
2025-01-23 14:42:20 -0600

    steam proxy server working

--- gamercred_api/steam_api/urls.py
+++ gamercred_api/steam_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/steam_user', views.get_steam_user, name='get_steam_user'),
+    path('api/steam_user/', views.get_steam_user, name='get_steam_user'),
 ]
