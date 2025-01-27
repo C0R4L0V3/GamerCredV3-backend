891d9a63b6b1b0c8b84232521881e1cd82da2218
C0R4L0V3 <coraline.e.love@gmail.com>
2025-01-27 12:50:46 -0600

    added backend pather fro filtering out player report results

--- gamercred_api/reports_api/views.py
+++ gamercred_api/reports_api/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework import viewsets
 from .serializers import ReportSerializer
 from .models import ReportSchema
+
 
 # Create your views here.
 class ReportViewSet(viewsets.ModelViewSet):
     queryset = ReportSchema.objects.all().order_by('id')
     serializer_class = ReportSerializer
+
+
+def get_reports_by_player(request):
+    player_reported = request.GET.get('player_reported')
+    if not player_reported:
+        return JsonResponse({'error': 'Player Id required'}, status=400)
+    
+    reports = ReportSchema.objects.filter(player_reported=player_reported)
+    if not reports.exists():
+        return JsonResponse({'nessage': 'No Reports found for the specifed player'}, status=400)
+    
+    reports_data = list(reports.values())
+    return JsonResponse({'reports about player': reports_data}, status=200)
